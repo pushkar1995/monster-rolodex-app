66e600fb0a2d95e49015b8df09770c318cb659a5
Pushkar Gautam <pushkar.gautam1994@gmail.com>
2019-09-08 21:02:23 +0545

    basic app with filter and components functionality

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './search-box.styles.css';
+
+const SearchBox = ({ placeholder, handleChange }) => {
+	return <input type="search" placeholder={placeholder} onChange={handleChange} className="search" />;
+};
+
+export default SearchBox;
